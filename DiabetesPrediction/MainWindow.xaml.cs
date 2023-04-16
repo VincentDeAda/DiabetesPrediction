560de26023ccfc53f1e18d40767273c49e2a56d2
Captain Sh4dow <CaptainSh4dow@gmail.com>
2023-04-16 12:06:11 +0300

    Add project files.

---
+++ DiabetesPrediction/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DiabetesPrediction;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+  
+
+    private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        var txtBox = (TextBox)e.Source;
+        var isValid= double.TryParse(txtBox.Text, out _);
+        if (!isValid) txtBox.Text = 0.ToString();
+    }
+}
---
+++ DiabetesPrediction/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DiabetesPrediction.ML;
+using System;
+using static DiabetesPrediction.ML.DiabetesPredictions;
+
+namespace DiabetesPrediction.ViewModels;
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty,NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    private bool isBusy = false;
+
+
+    [ObservableProperty]
+    private bool isNotBusy = true;
+
+
+    [ObservableProperty]
+    private double resultPercentage;
+
+    [ObservableProperty]
+    private ModelInput model = new ModelInput();
+
+    [RelayCommand]
+    public void CheckResult()
+    {
+        IsBusy = true;
+        var predictionResult = DiabetesPredictions.Predict(Model);
+        ResultPercentage = Math.Round(predictionResult.Score[0] * 100, 2);
+        IsBusy = false;
+
+    }
+
+}
